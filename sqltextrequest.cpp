0477ba15461e2dffa45815003fea6977a2079ee6
kevinall05 <allemandkevin1@gmail.com>
2022-02-25 16:55:11 +0100

    Finalisation de la partie requête en ligne de commandes

--- sqltextrequest.cpp
+++ sqltextrequest.cpp
@@ -1,4 +1,5 @@
 #include <QSqlRecord>
+#include <QSqlQuery>
 #include <QDebug>
 
 // insertion de la mainWindow
@@ -7,9 +8,50 @@
 
 void MainWindow::on_pushButtonSqlExecut_clicked()
 {
+    // on initialise le tableau
+    ui->tableWidgetSelect->clear();
+    ui->tableWidgetSelect->setRowCount(0);
+    ui->tableWidgetSelect->setColumnCount(0);
+    ui->tableWidgetSelect->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    // on récupère ce que l'utilisateur a saisi
     QString txtSqlRequest = ui->plainTextEditSelectRequest->toPlainText();
     qDebug()<<txtSqlRequest;
-    if (txtSqlRequest.contains("SELECT")) {
-       // ui->tabWidgetSelect->setVisible(1);
+
+    QSqlQuery sqlRequest(txtSqlRequest);
+    QSqlRecord recordRequest = sqlRequest.record();
+
+    // si la requête contient SELECT
+    if (txtSqlRequest.contains("SELECT") || txtSqlRequest.contains("select")) {
+
+       // on affiche le tableau pour les SELECT
+        ui->tableWidgetSelect->setVisible(1);
+        ui->tableWidgetSelect->setColumnCount(recordRequest.count());
+
+        int rows = 0;
+
+        // création du tableau
+        while(sqlRequest.next()) {
+            // on insère la ligne
+            ui->tableWidgetSelect->insertRow(rows);
+            for(int counter = 0; counter < recordRequest.count(); counter++) {
+                // on affiche l'item
+                ui->tableWidgetSelect->setItem(rows,counter,new QTableWidgetItem(sqlRequest.value(counter).toString()));
+                ui->tableWidgetSelect->setHorizontalHeaderItem(counter,new QTableWidgetItem(recordRequest.fieldName(counter)));
+            }
+            // on ajoute une ligne
+            rows+=1;
+        }
+
+        qDebug()<<"On réactive le tableau";
+    }
+
+    // on exécute la requête
+    if(sqlRequest.exec()) {
+        // la requête est fonctionnelle
+        ui->statusBar->showMessage("La requête a bien été exécuté",2000);
+    } else {
+        // la requête n'est pas fonctionnelle
+        ui->statusBar->showMessage("Erreur: vérifier la syntaxe",2000);
     }
 }
