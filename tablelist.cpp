9188c5a15ff6e66845bbec9b3f3ee7b97dfcafb8
kevinall05 <allemandkevin1@gmail.com>
2022-02-28 11:22:20 +0100

    Ajout de la fonction pour la suppression d'une ligne

--- tablelist.cpp
+++ tablelist.cpp
@@ -26,28 +26,34 @@ void MainWindow::on_listWidgetTablesList_itemClicked(QListWidgetItem *item)
     ui->tableWidgetStructure->setColumnCount(0);
     ui->tableWidgetStructure->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 
- QString txtReq;
- txtReq ="SELECT * FROM "+ui->listWidgetTablesList->currentItem()->text();
- QSqlQuery req(txtReq);
- qDebug()<<txtReq;
- int compteur= 0;
- QSqlRecord recordRequest = req.record();
- int nbChamp=recordRequest.count();
- ui->tableWidgetStructure->setColumnCount(nbChamp);
- //affichage des noms de colonne
- for(int noCol=0;noCol<nbChamp;noCol++)
- {
-     ui->tableWidgetStructure->setHorizontalHeaderItem(noCol,new QTableWidgetItem(recordRequest.fieldName(noCol)));
- }
+    // requête SQL
+     QString txtReq= "SELECT * FROM "+ui->listWidgetTablesList->currentItem()->text();
+     QSqlQuery req(txtReq);
+     qDebug()<<txtReq;
+
+     int compteur= 0;
+     QSqlRecord recordRequest = req.record();
+     int nbChamp=recordRequest.count();
+     ui->tableWidgetStructure->setColumnCount(nbChamp);
+     ui->tableWidgetDeleteEntry->setColumnCount(nbChamp);
+
+     //affichage des noms de colonne
+     for(int noCol=0;noCol<nbChamp;noCol++)
+     {
+         ui->tableWidgetStructure->setHorizontalHeaderItem(noCol,new QTableWidgetItem(recordRequest.fieldName(noCol)));
+         ui->tableWidgetDeleteEntry->setHorizontalHeaderItem(noCol,new QTableWidgetItem(recordRequest.fieldName(noCol)));
+     }
 
  //affichage des valeurs
  while(req.next())
  {
      // on insère la ligne
          ui->tableWidgetStructure->setRowCount(compteur+1);
+         ui->tableWidgetDeleteEntry->setRowCount(compteur+1);
          for(int counter = 0; counter < recordRequest.count(); counter++) {
              // on affiche l'item
              ui->tableWidgetStructure->setItem(compteur,counter,new QTableWidgetItem(req.value(counter).toString()));
+             ui->tableWidgetDeleteEntry->setItem(compteur,counter,new QTableWidgetItem(req.value(counter).toString()));
          }
          // on ajoute une ligne
          compteur+=1;
